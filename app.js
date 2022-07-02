e7bb86e9fea5b239d9d8e4e25aa2eec1206a1597
Henrique Ferraz de Oliveira <henrique.oliveira@calindra.com.br>
2022-07-02 16:29:01 -0300

    feat: responde apenas ao primeiro link do array e da erro: '[200~ Não é possível definir cabeçalhos após serem enviados ao cliente~'

--- app.js
+++ app.js
@@ -3,50 +3,52 @@ const bodyParser = require('body-parser')
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3001;
-const url = "https://swapi.dev/api/" 
+const url = "https://swapi.dev/api/"
 
 app.use(express.json());
 
 const urlEntitiesFilms = {
-    characters : "characters",
-    planets : "planets",
-    starships : "starships",
-    vehicles : "vehicles",
-    species : "species"
+    characters: "characters",
+    planets: "planets",
+    starships: "starships",
+    vehicles: "vehicles",
+    species: "species"
 }
 
-app.get(`/:entity/:id`, (req, res) => {
-    const {entity, id} = req.params;
+
+app.get('/:entity/:id', (req, res) => {
+    const { entity, id } = req.params;
     const enrichFields = req.query.enrichFields
     const urlResquest = `${url}/${entity}/${id}/?enrichFields=${enrichFields}`;
-    
+
     axios.get(urlResquest)
-    .then((response) => {
-        let arrayResponse = [];
-        let responseDateValue = response.data[`${enrichFields}`];
+        .then((response) => {
+            let arrayResponse = [];
+            let responseDataValue = response.data[`${enrichFields}`]
 
-        if(enrichFields == urlEntitiesFilms[`${enrichFields}`] ) {
-            for(let i = 0; i < responseDateValue.length ; i++) {
-                arrayResponse.push(urlResquest)
-                console.log(i)
-                console.log(arrayResponse)
+            if (enrichFields == urlEntitiesFilms[`${enrichFields}`]) {
+                for (let i = 0; i <= responseDataValue.length; i++) {
+                    console.log(responseDataValue[i])
+
+                    axios.get(responseDataValue[i])
+                        .then((responseValueUrlApi) => {
+                            res.json(responseValueUrlApi.data);
+                            console.log(responseValueUrlApi.data);
+                        }).catch((err) => {
+                            console.log(err);
+                         });
+                    console.log(i)
+                }
+
             }
-            res.send({ resultado : response.data[`${enrichFields}`]});
-            console.log(response.data[`${enrichFields}`]);
-
-
-        }
-        
-        // res.status(200).json(response.data)
-    }).catch((erro) => {
-        console.log(erro)
-        res.send("ERRO 500");
-    }) 
-
-});
+        }).catch((erro) => {
+            console.log(erro)
+            res.send("ERRO 500");
+        })
+    });
 
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
 
--- services/api.js
+++ services/api.js
@@ -1,4 +1,69 @@
-const axios = require("axios");
+const { default: axios } = require('axios');
+const bodyParser = require('body-parser')
+const express = require('express')
+const app = express()
+const port = 3010;
+const url = "https://swapi.dev/api/"
 
-const api = axios.get(`https://swapi.dev/api/planets/3/`)
+app.use(express.json());
 
+// const urlEntitiesFilms = ["characters", "planets", "starships", "vehicles", "species"]
+
+const urlEntitiesFilms = {
+    characters: "characters",
+    planets: "planets",
+    starships: "starships",
+    vehicles: "vehicles",
+    species: "species"
+}
+
+
+function splintando(value) {
+
+}
+
+app.get('/:entity/:id', (req, res) => {
+    const { entity, id } = req.params;
+    const enrichFields = req.query.enrichFields
+    const urlResquest = `${url}/${entity}/${id}/?enrichFields=${enrichFields}`;
+
+    axios.get(urlResquest)
+        .then((response) => {
+            let arrayResponse = [];
+            let responseArrayData = response.data
+            
+            if(enrichFields == urlEntitiesFilms.enrichFields) {
+                console.log(response.data)
+
+            // res.send({ resultado: response.data[`${enrichFields}`] });
+            // console.log(response.data[`${enrichFields}`]);
+            
+            
+        }
+        
+        // res.status(200).json(response.data)
+        }).catch((erro) => {
+            console.log(erro)
+            res.send("ERRO 500");
+        })
+        
+    });
+    
+    
+    app.listen(port, () => {
+        console.log(`Example app listening on port ${port}`)
+    })
+    
+    
+    // if (enrichFields == urlEntitiesFilms[`${enrichFields}`]) {
+    //     for (let i = 0; i <= responseDataValue.length; i++) {
+    //         console.log(responseDataValue[i])
+
+    //         axios.get(responseDataValue[i])
+    //             .then((responseValueUrlApi) => {
+    //                 console.log(responseValueUrlApi.data);
+    //             }).catch((err) => {
+    //                 console.log(err);
+    //             });
+    //         console.log(i)
+    //     }
